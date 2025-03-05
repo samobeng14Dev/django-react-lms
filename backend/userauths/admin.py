f06d90fa4c099ff0c1aa90210f727605a4defeb0
Samuel Obeng <obengsamuel270@gmail.com>
2025-03-05 02:02:05 +0000

    created a custom user and profile model

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from userauths.models import User, Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'country', 'date']
+    search_fields = ['user', 'full_name', 'country', 'date']
+    list_filter = ['date']
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+
--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create a custom your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    full_name = models.CharField(max_length=100)
+    otp = models.CharField(max_length=6, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+    # if not to check if the field is empty or not
+
+    def save(self, *args, **kwargs):
+        email_username, full_name = self.email.split('@')
+        if not self.full_name:
+            self.full_name = email_username
+        if not self.username:
+            self.username = email_username
+        super().save(*args, **kwargs)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to='user_folder',
+                             default='default.jpg', null=True, blank=True)
+    full_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.email)
+
+    # if not to check if the field is empty or not
+    def save(self, *args, **kwargs):
+        if not self.full_name:
+            self.full_name = self.user.username
+        super().save(*args, **kwargs)
