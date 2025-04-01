03f2a735d320a3f4dae5d7071221ca211ac8c0ae
Samuel Obeng <obengsamuel270@gmail.com>
2025-04-01 23:33:18 +0000

    fixed paypal client and secret id

--- backend/api/views.py
+++ backend/api/views.py
@@ -23,6 +23,8 @@ import stripe
 import requests
 
 stripe.api_key=settings.STRIPE_SECRET_KEY
+PAYPAL_CLIENT_ID = settings.PAYPAL_CLIENT_ID
+PAYPAL_SECRET_ID = settings.PAYPAL_SECRET_ID
 
 
 class MytokenObtainPairView(TokenObtainPairView):
